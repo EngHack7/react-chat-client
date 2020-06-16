50acdf1ab9785921ec867b71c2bccaf0883cc043
EngHack7 <hamzaz.k.hasan@st.ahu.edu.jo>
2020-06-17 01:19:11 +0300

    add build

---
+++ build/precache-manifest.e0efc23ba775a896d444ba5a302eed99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fc8b72dd419f3ebb98b5f5d6adc688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3b850e86f1dd7a708b7",
+    "url": "/static/css/main.b3acbf7f.chunk.css"
+  },
+  {
+    "revision": "9480f08231047d64cff9",
+    "url": "/static/js/2.dd37520f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.dd37520f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b850e86f1dd7a708b7",
+    "url": "/static/js/main.47a441dc.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
